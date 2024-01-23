450a315f3ef07d9a20cfeea29d1a7fde9ab57c62
Jordan-Day-ONS <57715292+Jday7879@users.noreply.github.com>
2024-01-23 14:30:17 +0000

    Partitions coalesce (#122)
    * creating folder and file in raw notebooks
    
    * Tip of the week layout
    
    * R code and converted into md to check layout
    
    * Fixed link issues on Union dataframe #119
    
    * Copy into Partitions notebook
    
    * Updated partitions notebook with outputs
    
    * Add in reasons for not using many small partitions
    
    * updated partitions markdown file
    
    * Converted reading and writing alongside partitions
    
    * corrected link to sparklyr-intro
    
    * revert changes to convert.py
    
    * Removed working version of the content
    
    ---------
    
    Co-authored-by: kellyn <Nathan.Kelly@ons.gov.uk>

--- ons-spark/raw-notebooks/partitions/r_input.R
+++ ons-spark/raw-notebooks/partitions/r_input.R
@@ -149,14 +149,53 @@ print_partitioning_info(grouped_df)
 
 print_partitioning_info(window_df)
 
-repartition_path <- paste0(config$checkpoint_path, "/rescue_by_year.parquet")
-sparklyr::spark_write_parquet(rescue, 
-                              repartition_path,
-                              mode='overwrite',
-                              partition_by='CalYear')
 
-cmd <- paste0("hdfs dfs -ls -C ", repartition_path)
-system(cmd)
+# Stop previous spark session and create new spark session limiting number of partitions
+spark_disconnect(sc)
 
-cmd <- paste0("hdfs dfs -rm -r -skipTrash ", repartition_path)
-system(cmd)
+small_config <- sparklyr::spark_config()
+small_config$spark.sql.shuffle.partitions <- 12
+
+sc <- sparklyr::spark_connect(
+  master = "local",
+  app_name = "partitions",
+  config = small_config)
+
+sparklyr::spark_connection_is_open(sc)
+
+# Set the data path
+config <- yaml::yaml.load_file("ons-spark/config.yaml")
+# Read in and shuffle data
+rescue <- sparklyr::spark_read_csv(sc, config$rescue_path_csv, header=TRUE, infer_schema=TRUE)
+
+rescue <- rescue %>%
+    dplyr::rename(
+        incident_number = IncidentNumber,
+        animal_group = AnimalGroupParent,
+        cal_year = CalYear) %>%
+        dplyr::group_by(animal_group,cal_year) %>%
+        dplyr::count(animal_count = count())
+
+rescue %>% head(5)
+
+print(paste0("Rescue partitions: ", sparklyr::sdf_num_partitions(rescue)))
+
+dogs <- rescue %>% sparklyr::filter(animal_group == 'Dog')
+cats <- rescue %>% sparklyr::filter(animal_group == 'Cat')
+hamsters <- rescue %>% sparklyr::filter(animal_group == 'Hamster')
+
+dogs %>% head(5)
+
+print(paste0("Dogs partitions: ", sparklyr::sdf_num_partitions(dogs)))
+print(paste0("Cats partitions: ", sparklyr::sdf_num_partitions(cats)))
+print(paste0("Hamsters partitions: ", sparklyr::sdf_num_partitions(hamsters)))
+
+dogs_and_cats = sparklyr::sdf_bind_rows(dogs,cats)
+print(paste0("Dogs and Cats union partitions: ", sparklyr::sdf_num_partitions(dogs_and_cats)))
+
+dogs_cats_and_hamsters = sparklyr::sdf_bind_rows(dogs_and_cats,hamsters)
+print(paste0("Dogs, Cats and Hamsters union partitions: ", sdf_num_partitions(dogs_cats_and_hamsters)))
+
+sparklyr::sdf_num_partitions(dogs_cats_and_hamsters %>% sparklyr::sdf_sort(c('animal_group','cal_year')))
+
+sparklyr::sdf_num_partitions(dogs_cats_and_hamsters %>% sparklyr::sdf_repartition(20))
--- ons-spark/raw-notebooks/reading-and-writing-data/r_input.R
+++ ons-spark/raw-notebooks/reading-and-writing-data/r_input.R
@@ -44,6 +44,24 @@ rescue_df <- sparklyr::spark_read_orc(sc, path = config$rescue_path_orc)
 
 sparklyr::spark_write_orc(rescue_df, paste0(config$temp_outputs, "animal_rescue_r.orc"), mode = 'overwrite')
 
+library(sparkavro)
+
+animal_rescue = sparkavro::spark_read_avro(sc, "animal_rescue", config$rescue_path_avro)
+
+sparkavro::spark_write_avro(animal_rescue, paste0(config$temp_outputs, "animal_rescue.avro"), mode = "overwrite")
+
 rescue_df <- sparklyr::sdf_sql(sc, "SELECT * FROM train_tmp.animal_rescue")
 
 sparklyr::spark_write_table(rescue_df, name = "train_tmp.animal_rescue_temp", mode = 'overwrite')
+
+repartition_path <- paste0(config$checkpoint_path, "/rescue_by_year.parquet")
+sparklyr::spark_write_parquet(rescue_df, 
+                              repartition_path,
+                              mode='overwrite',
+                              partition_by='cal_year')
+
+cmd <- paste0("hdfs dfs -ls -C ", repartition_path)
+system(cmd)
+
+cmd <- paste0("hdfs dfs -rm -r -skipTrash ", repartition_path)
+system(cmd)
